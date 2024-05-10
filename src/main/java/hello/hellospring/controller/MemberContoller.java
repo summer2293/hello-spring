425b86a3ede4e8ab1a687d70e4e4cf8740290864
summer2293 <s>
2024-05-10 15:42:39 +0900

    chap05 1,2,3

--- src/main/java/hello/hellospring/controller/MemberContoller.java
+++ src/main/java/hello/hellospring/controller/MemberContoller.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // 스프링이 컨테이너 어노테이션을 걸면
 // 스프링이 실행할때 객체를 생성해서 가지고있다.
@@ -26,4 +32,27 @@ public class MemberContoller {
     public MemberContoller(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        System.out.println("member:"+member.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "memberList";
+    }
+
 }
---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
